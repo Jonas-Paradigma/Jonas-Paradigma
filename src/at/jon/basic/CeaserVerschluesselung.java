b81e78d29622ee0e9cf6c23a06630237ee13869f
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2023-10-11 09:28:51 +0200

    wuerfel

---
+++ src/at/jon/basic/CeaserVerschluesselung.java
@@ -0,0 +1,14 @@
+package at.jon.basic;
+
+public class CeaserVerschluesselung {
+    public static void main(String[] args) {
+    String result = ecrypt(1, "Hallo");
+    }
+
+    public static String ecrypt(int shift, String stringToEncrypt){
+
+        char[] inputData = stringToEncrypt.toCharArray();
+
+        return "";
+    }
+}
