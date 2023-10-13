3cae58bd981f15a14c773ffde59cb341ece9e370
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2023-10-13 09:22:54 +0200

    wuerfel

--- src/at/jon/basic/BankautomatBeispiel.java
+++ src/at/jon/basic/BankautomatBeispiel.java
@@ -51,3 +51,4 @@ public class BankautomatBeispiel {
         System.out.println("Beendet");
     }
 }
+//Fertig
---
+++ src/at/jon/basic/StringHelper.java
@@ -0,0 +1,10 @@
+package at.jon.basic;
+
+public class StringHelper {
+    public static boolean isPalindrome (String text) {
+
+
+
+        return false;
+    }
+}
