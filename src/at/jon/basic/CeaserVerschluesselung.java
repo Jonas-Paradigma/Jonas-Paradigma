a564bf83d5afa7de1b9c5cb497bb6eb38d23c65b
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2023-10-18 08:41:17 +0200

    wuerfel

--- src/at/jon/basic/CeaserVerschluesselung.java
+++ src/at/jon/basic/CeaserVerschluesselung.java
@@ -2,23 +2,29 @@ package at.jon.basic;
 
 public class CeaserVerschluesselung {
     public static void main(String[] args) {
-    String result = ecrypt(3, "hallo");
+        String result = encrypt(1, "zzz");
         System.out.println(result);
     }
 
-    public static String ecrypt(int shift, String stringToEncrypt){
-
-
+    public static String encrypt(int shift, String stringToEncrypt) {
         String encryptedString = "";
         char[] inputData = stringToEncrypt.toCharArray();
 
-        for(int i = 0; i < inputData.length;i++)
-        {
-            int letter = inputData[i] + shift;
-            encryptedString += Character.toString(letter);
+        for (int i = 0; i < inputData.length; i++) {
+            char letter = inputData[i];
 
+            if (Character.isLetter(letter)) {
+                char base = Character.isUpperCase(letter) ? 'A' : 'a';
+                int shifted = (letter - base + shift) % 26;
+                if (shifted < 0) {
+                    shifted += 26;
+                }
+                encryptedString += (char) (base + shifted);
+            } else {
+                encryptedString += letter;
+            }
         }
 
         return encryptedString;
-    }
+}
 }
--- src/at/jon/basic/StringHelper.java
+++ src/at/jon/basic/StringHelper.java
@@ -1,10 +1,33 @@
 package at.jon.basic;
 
 public class StringHelper {
+
+    public static void main(String[] args) {
+    int result = countletters("Hans achtet auf mich", 'a');
+        System.out.println(result);
+    }
     public static boolean isPalindrome (String text) {
-
-
+        //A n n A
 
         return false;
+    }
+
+    public static int countletters(String text, char search) {
+        //Hans
+
+        //0 1 2 3
+        //H a n s
+        char[] textArray = text.toCharArray();
+        int count = 0;
+
+
+        for (int i = 0; i < textArray.length ; i++){
+            if(textArray[i]==search){
+                count++;
+            }
+
+        }
+
+        return -1;
     }
 }
